cbaf4cffebebd316ff965303d111c8f540675fef
Awesomewsdm <88857387+Awesomewsdm@users.noreply.github.com>
2023-01-04 22:56:13 +0000

    a

--- lib/src/repository/authentification/user_booking.dart
+++ lib/src/repository/authentification/user_booking.dart
@@ -1,3 +1,20 @@
+import 'package:angie_notebook/src/common_widgets/destination_seats_layouts/economy__seats_layout/accra_seats_layout.dart';
+import 'package:angie_notebook/src/common_widgets/destination_seats_layouts/economy__seats_layout/cape_coast_seats_layout.dart';
+import 'package:angie_notebook/src/common_widgets/destination_seats_layouts/economy__seats_layout/kasoa_seats_layout.dart';
+import 'package:angie_notebook/src/common_widgets/destination_seats_layouts/economy__seats_layout/koforidua_seats_layout.dart';
+import 'package:angie_notebook/src/common_widgets/destination_seats_layouts/economy__seats_layout/sunyani_seats_layout.dart';
+import 'package:angie_notebook/src/common_widgets/destination_seats_layouts/economy__seats_layout/takoradi_seats_layout.dart';
+import 'package:angie_notebook/src/common_widgets/destination_seats_layouts/economy__seats_layout/tema_seats_layout.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../../common_widgets/destination_seats_layouts/executive_seats_layout/accra_seats_layout.dart';
+import '../../common_widgets/destination_seats_layouts/executive_seats_layout/cape_coast_seats_layout.dart';
+import '../../common_widgets/destination_seats_layouts/executive_seats_layout/kasoa_seats_layout.dart';
+import '../../common_widgets/destination_seats_layouts/executive_seats_layout/koforidua_seats_layout.dart';
+import '../../common_widgets/destination_seats_layouts/executive_seats_layout/sunyani_seats_layout.dart';
+import '../../common_widgets/destination_seats_layouts/executive_seats_layout/takoradi_seats_layout.dart';
+import '../../common_widgets/destination_seats_layouts/executive_seats_layout/tema_seats_layout.dart';
+
 class UserBooking {
   final int selectedSeats;
   final String selectedbusClass;
@@ -11,21 +28,58 @@ class UserBooking {
     'Cape Coast': 90,
     'Koforidua': 80,
     'Sunyani': 100,
-   
   };
-   final executiveSeatPrice = {
+  final executiveSeatPrice = {
     'Accra': 120,
     'Tema': 130,
     'Kasoa': 120,
     'Cape Coast': 100,
     'Koforidua': 100,
     'Sunyani': 110,
-
   };
 
-  
+  Widget changeSeatsLayout(String selectedDestination) {
+    if (selectedDestination == 'Accra') {
+      return AccraEconomySeatLayout();
+    } else if (selectedDestination == 'Tema') {
+      return TemaEconomySeatLayout();
+    } else if (selectedDestination == 'Takoradi') {
+      return TakoradiEconomySeatLayout();
+    } else if (selectedDestination == 'Cape Coast') {
+      return CapeCoastEconomySeatLayout();
+    } else if (selectedDestination == 'Sunyani') {
+      return SunyaniEconomySeatLayout();
+    } else if (selectedDestination == 'Kasoa') {
+      return KasoaEconomySeatLayout();
+    } else if (selectedDestination == 'Koforidua') {
+      return KoforiduaEconomySeatLayout();
+    }
 
-  void calcPrice() {
-    // var seatPrice = selectedSeats * executiveSeatPrice.values ;
+    return AccraEconomySeatLayout();
   }
 }
+   
+   
+  Widget changeSeatsLayout(String selectedDestination) {
+    if (selectedDestination == 'Accra') {
+      return AccraExecutiveSeatLayout();
+    } else if (selectedDestination == 'Tema') {
+      return TemaExecutiveSeatLayout();
+    } else if (selectedDestination == 'Takoradi') {
+      return TakoradiExecutiveSeatLayout();
+    } else if (selectedDestination == 'Cape Coast') {
+      return CapeCoastExecutiveSeatLayout();
+    } else if (selectedDestination == 'Sunyani') {
+      return SunyaniExecutiveSeatLayout();
+    } else if (selectedDestination == 'Kasoa') {
+      return KasoaExecutiveSeatLayout();
+    } else if (selectedDestination == 'Koforidua') {
+      return KoforiduaExecutiveSeatLayout();
+    }
+
+    return AccraExecutiveSeatLayout();
+  }
+
+ // void calcPrice() {
+    //   // var seatPrice = selectedSeats * executiveSeatPrice.values ;
+    // }
